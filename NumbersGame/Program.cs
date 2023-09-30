922bc314db785a8bce95a02a796989903ce6bfba
andritox55 <andresww.55@hotmail.com>
2023-09-30 16:48:45 +0200

    Add project files.

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace NumbersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Random random = new Random();
+
+            int number = random.Next(1, 20);
+
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+
+            // run as long as user input is equal to random number or 5 attempts
+
+            int count = 0;
+            while (count != 5)
+            {
+              
+                String input = Console.ReadLine();
+
+                int choice = int.Parse(input);
+
+                if (number == choice)
+                {
+                    Console.WriteLine("Wohoo! Du klarade det!");
+                    break;
+                }
+                else if (choice < number)
+                {
+                    Console.WriteLine("Tyvärr, du gissade för lågt!");
+                    count++;
+                }
+                else if (choice > number)
+                {
+                    Console.WriteLine("Tyvärr, du gissade för högt!");
+                    count++;
+                }
+                
+                if (count == 5)
+                {
+                    Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök!");
+                }
+            }
+        }
+    }
+}
